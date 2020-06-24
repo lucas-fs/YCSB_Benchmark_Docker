657e1111ebfa6019297a09db5a852a72af2b97cc
Lucas Ferreira da Silva <lferreira@inf.ufsm.br>
2020-06-24 03:39:15 -0300

    update scripts

--- execute.sh
+++ execute.sh
@@ -2,8 +2,9 @@
 
 workload=$1
 recordcount=$2
+replicas=$3
 
 node_ip=`getent hosts tasks.seed | awk '{print $1}'| head -n 1`
 echo "seed node ip: $node_ip"
 
-ycsb run cassandra-cql -s -p hosts="$node_ip" -P $YCSB_HOME/workloads/workload$workload -p recordcount=$recordcount -cp $CLASSPATH
+$YCSB_HOME/bin/ycsb run cassandra-cql -s -p hosts="$node_ip" -P $YCSB_HOME/workloads/workload$workload -p recordcount=$recordcount -cp $CLASSPATH > /ycsb_output/out-$replicas-$recordcount-$workload.txt 2>&1
--- load.sh
+++ load.sh
@@ -2,7 +2,9 @@
 
 workload=$1
 recordcount=$2
+replicas=$3
 
+echo "geting seed ip..."
 node_ip=`getent hosts tasks.seed | awk '{print $1}'| head -n 1`
 echo "seed node ip: $node_ip"
 
@@ -25,4 +27,4 @@ cqlsh $node_ip -p9042 --cqlversion=3.4.4 -e "create keyspace ycsb
         field8 varchar,
         field9 varchar);"
 
-ycsb load cassandra-cql -s -p hosts="$node_ip" -P $YCSB_HOME/workloads/workload$workload -p recordcount=$recordcount -cp $CLASSPATH
+$YCSB_HOME/bin/ycsb load cassandra-cql -s -p hosts="$node_ip" -P $YCSB_HOME/workloads/workload$workload -p recordcount=$recordcount -cp $CLASSPATH > /ycsb_output/out-$replicas-$recordcount-$workload.txt 2>&1
