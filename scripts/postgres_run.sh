7b23321ec7b0030e79dd0ca42b0880fab669e91f
Lucas Ferreira da Silva <lferreira@inf.ufsm.br>
2021-02-08 16:44:29 -0300

    change Dockerfile and clean repository

---
+++ scripts/postgres_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+node_ip=$1
+workload=$2
+recordcount=$3
+replicas=$4
+exe=$5
+
+echo "[RUN Script] replicas:$replicas workload:$workload recordcount:$recordcount exec:$exe"
+$YCSB_HOME/bin/ycsb run jdbc -s -P $YCSB_HOME/workloads/workloada -P $JDBC_HOME/conf/db.properties -cp $CLASSPATH -p recordcount=$recordcount -p operationcount=10000 > /ycsb_output/out-$replicas-$recordcount-$workload-$exe.txt 2>&1
+chmod 777 /ycsb_output/out-$replicas-$recordcount-$workload-$exe.txt
+
+echo "[RUN Script] Finished!"
