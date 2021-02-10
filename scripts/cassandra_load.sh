5daf423c42bfb87097e9d4714ca25b1f3456ef20
Lucas Ferreira da Silva <lferreira@inf.ufsm.br>
2021-02-10 02:33:59 -0300

    update cassandra scripts

--- scripts/cassandra_load.sh
+++ scripts/cassandra_load.sh
@@ -7,32 +7,46 @@ replicas=$4
 
 
 echo "[LOAD Script] Dropping ycsb keyspace"
-cqlsh $node_ip -p9042 --cqlversion=3.4.4 --connect-timeout=10 -e "DROP keyspace IF EXISTS ycsb;"
+cqlsh $node_ip -p9042 --cqlversion=3.4.4 --connect-timeout=10 \
+    -e "DROP keyspace IF EXISTS ycsb;"
 
-sleep 5
+sleep 10
 
 echo "[LOAD Script] Recreating ycsb keyspace"
-cqlsh $node_ip -p9042 --cqlversion=3.4.4 --connect-timeout=10 -e "create keyspace ycsb
-        WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 3 }; 
-        USE ycsb; 
-        create table usertable (
-        y_id varchar primary key,
-        field0 varchar,
-        field1 varchar,
-        field2 varchar,
-        field3 varchar,
-        field4 varchar,
-        field5 varchar,
-        field6 varchar,
-        field7 varchar,
-        field8 varchar,
-        field9 varchar);"
+cqlsh $node_ip -p9042 --cqlversion=3.4.4 --connect-timeout=10 \
+    -e "create keyspace ycsb
+    WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 3 }; 
+    USE ycsb; 
+    create table usertable (
+    y_id varchar primary key,
+    field0 varchar,
+    field1 varchar,
+    field2 varchar,
+    field3 varchar,
+    field4 varchar,
+    field5 varchar,
+    field6 varchar,
+    field7 varchar,
+    field8 varchar,
+    field9 varchar);"
 
-sleep 5
+sleep 10
 
 echo "[LOAD Script] Performing load..."
-$YCSB_HOME/bin/ycsb load cassandra-cql -s -p hosts="$node_ip" -P $YCSB_HOME/workloads/workload$workload -p recordcount=$recordcount -p operationcount=10000 -p rectime=3000 -cp $CLASSPATH > /ycsb_output/out-load-$replicas-$recordcount-$workload.txt 2>&1
 
-chmod 777 /ycsb_output/out-load-$replicas-$recordcount-$workload.txt
+$YCSB_HOME/bin/ycsb load cassandra-cql -s \
+-p hosts="$node_ip" \
+-P $YCSB_HOME/workloads/workload$workload \
+-p recordcount=$recordcount \
+-p operationcount=10000 \
+-p rectime=3000 \
+-p cassandra.connecttimeoutmillis=60000 \
+-p cassandra.readtimeoutmillis=60000 \
+-p cassandra.readconsistencylevel=QUORUM \
+-p cassandra.writeconsistencylevel=QUORUM \
+-cp $CLASSPATH \
+> /ycsb_outputs/out-load-$replicas-$recordcount-$workload.txt 2>&1
+
+chmod 777 /ycsb_outputs/out-load-$replicas-$recordcount-$workload.txt
 
 echo "[Load Script] Load finished!"
--- scripts/cassandra_run.sh
+++ scripts/cassandra_run.sh
@@ -6,9 +6,22 @@ recordcount=$3
 replicas=$4
 exe=$5
 
-echo "[RUN Script] replicas:$replicas workload:$workload recordcount:$recordcount exec:$exe"
-$YCSB_HOME/bin/ycsb run cassandra-cql -s -p hosts="$node_ip" -P $YCSB_HOME/workloads/workload$workload -p recordcount=$recordcount -p rectime=3000 -p operationcount=10000 -cp $CLASSPATH > /ycsb_output/out-$replicas-$recordcount-$workload-$exe.txt 2>&1
+echo "[RUN Script] replicas:$replicas workload:$workload \
+      recordcount:$recordcount exec:$exe"
 
-chmod 777 /ycsb_output/out-$replicas-$recordcount-$workload-$exe.txt
+$YCSB_HOME/bin/ycsb run cassandra-cql -s \
+-p hosts="$node_ip" \
+-P $YCSB_HOME/workloads/workload$workload \
+-p recordcount=$recordcount \
+-p rectime=3000 \
+-p cassandra.connecttimeoutmillis=60000 \
+-p cassandra.readtimeoutmillis=60000 \
+-p cassandra.readconsistencylevel=QUORUM \
+-p cassandra.writeconsistencylevel=QUORUM \
+-p operationcount=10000 \
+-cp $CLASSPATH \
+> /ycsb_outputs/out-$replicas-$recordcount-$workload-$exe.txt 2>&1
+
+chmod 777 /ycsb_outputs/out-$replicas-$recordcount-$workload-$exe.txt
 
 echo "[RUN Script] Finished!"
