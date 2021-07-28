298409de48208111a3187d4a4b20a1f44adf811a
Lucas Ferreira da Silva <lferreira@inf.ufsm.br>
2021-07-28 02:12:05 -0300

    Call shard_table.sh in postgres shard load script

--- scripts/postgres_load_shard.sh
+++ scripts/postgres_load_shard.sh
@@ -5,10 +5,9 @@ node_ip=$1
 workload=$2
 recordcount=$3
 replicas=$4
+b_size=$5
 
 db_conf="# Properties file that contains database connection information.
-
-# jdbc.fetchsize=20
 db.driver=org.postgresql.Driver
 db.url=jdbc:postgresql://$node_ip:5432/ycsb
 db.user=postgres
@@ -18,19 +17,13 @@ db.passwd=postgres
 rm $JDBC_HOME/conf/db.properties
 echo -e "$db_conf" > $JDBC_HOME/conf/db.properties
 
-echo "[LOAD Script] Dropping table usertable"
-PGPASSWORD=postgres psql -h $node_ip -U postgres -d ycsb -c "DROP TABLE IF EXISTS usertable;"
-
-sleep 5 
-
-echo "[LOAD Script] Recreating table usertable"
-java -cp $JDBC_HOME/lib/jdbc-binding-$YCSB_VERSION-SNAPSHOT.jar:$JDBC_HOME/lib/postgresql-42.2.14.jar site.ycsb.db.JdbcDBCreateTable -P $JDBC_HOME/conf/db.properties -n usertable
-
-sleep 5
+echo "[LOAD Script] Call shard_table.sh script to create usertable sharded"
+./shard_table.sh postgres postgres $node_ip $replicas
+sleep 300
 
 echo "[LOAD Script] Performing load..."
-$YCSB_HOME/bin/ycsb load jdbc -s -P $YCSB_HOME/workloads/workload$workload -P $JDBC_HOME/conf/db.properties -cp $CLASSPATH -p recordcount=$recordcount -p operationcount=10000 > /ycsb_output/out-load-$replicas-$recordcount-$workload.txt 2>&1
+$YCSB_HOME/bin/ycsb load jdbc -s -P $YCSB_HOME/workloads/workload$workload -P $JDBC_HOME/conf/db.properties -cp $CLASSPATH -p recordcount=$recordcount -p operationcount=10000 > /ycsb_output/out-load-$replicas-$recordcount-$workload-$b_size.txt 2>&1
 
-chmod 777 /ycsb_output/out-load-$replicas-$recordcount-$workload.txt
+chmod 777 /ycsb_output/out-load-$replicas-$recordcount-$workload-$b_size.txt
 
 echo "[LOAD Script] Load finished!"
