b8486b95c92a4b8d97e0d9c1238f698eccd7a3a5
Lucas Ferreira da Silva <lferreira@inf.ufsm.br>
2021-08-08 23:21:27 -0300

    update shard scripts

--- scripts/postgres_run.sh
+++ scripts/postgres_run.sh
@@ -5,9 +5,48 @@ workload=$2
 recordcount=$3
 replicas=$4
 exe=$5
+b_size=$6
+f_size=$7
+
+# master_node=$(getent hosts tasks.master | awk '{print $1}'| head -n 1)
+# shard_nodes=(`getent hosts tasks.shard | awk '{print $1}'`)
+# port="5432"
+
+# db_url="jdbc:postgresql://$master_node:$port/ycsb?reWriteBatchedInserts=true;"
+# shards_count=${#shard_nodes[@]}
+
+# for (( i=0; i<$shards_count-1; i++ ))
+# do
+#     db_url+="jdbc:postgresql://${shard_nodes[$i]}:$port/ycsb?reWriteBatchedInserts=true;"
+# done
+# db_url+="jdbc:postgresql://${shard_nodes[$i]}:$port/ycsb?reWriteBatchedInserts=true"
+
+db_conf="# Properties file that contains database connection information.
+db.driver=org.postgresql.Driver
+db.url=jdbc:postgresql://$node_ip:5432/ycsb?reWriteBatchedInserts=true
+db.user=postgres
+db.passwd=postgres
+db.batchsize=$b_size
+jdbc.fetchsize=$f_size
+jdbc.batchupdateapi=true
+jdbc.autocommit=false
+"
+
+# db_conf="# Properties file that contains database connection information.
+# db.driver=org.postgresql.Driver
+# db.url=jdbc:postgresql://$node_ip:5432/ycsb?reWriteBatchedInserts=true
+# db.user=postgres
+# db.passwd=postgres
+# db.batchsize=$b_size
+# jdbc.batchupdateapi=true
+# "
+
+rm $JDBC_HOME/conf/db.properties
+echo -e "$db_conf" > $JDBC_HOME/conf/db.properties
+sleep 10
 
 echo "[RUN Script] replicas:$replicas workload:$workload recordcount:$recordcount exec:$exe"
-$YCSB_HOME/bin/ycsb run jdbc -s -P $YCSB_HOME/workloads/workloada -P $JDBC_HOME/conf/db.properties -cp $CLASSPATH -p recordcount=$recordcount -p operationcount=10000 > /ycsb_output/out-$replicas-$recordcount-$workload-$exe.txt 2>&1
-chmod 777 /ycsb_output/out-$replicas-$recordcount-$workload-$exe.txt
+$YCSB_HOME/bin/ycsb run jdbc -s -P $YCSB_HOME/workloads/workloada -P $JDBC_HOME/conf/db.properties -cp $CLASSPATH -p recordcount=$recordcount -p operationcount=10000 > /ycsb_outputs/postgres-out-$replicas-$recordcount-$workload-$exe-bs$b_size-fs$f_size.txt 2>&1
+chmod 777 /ycsb_outputs/postgres-out-$replicas-$recordcount-$workload-$exe-bs$b_size-fs$f_size.txt
 
 echo "[RUN Script] Finished!"
